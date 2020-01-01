52b0f3795113b7df95e1b66a1d24eb6c13ca7b17
HHR <467178874@qq.com>
2020-01-01 23:12:36 +0800

    缝缝补补

--- class2ics.py
+++ class2ics.py
@@ -26,7 +26,7 @@ def calcStartDate(week, weekday, FirstDay):
 
 def calcEndDate(week, weekday, FirstDay):
     LastDay = FirstDay + datetime.timedelta(7 * int(week.split("-")[1].split("\\u546")[0]) - 7)
-    LastDay = LastDay + datetime.timedelta(int(weekday) - 1)
+    LastDay = LastDay + datetime.timedelta(int(weekday))
     return LastDay.strftime("%Y%m%d")
 
 
@@ -74,6 +74,7 @@ if __name__ == '__main__':
     url = "http://api.jh.zjut.edu.cn/student/classZf.php?username=" + input("请输入学号:") + "&password=" + input(
         "请输入密码:") + "&year=" + input("请输入学年:") + "&term=" + calcSemester(input("请输入学期：1-上学期 2-下学期 3-短学期:"))
     content = requests.get(url, headers=headers).content.decode().encode("GBK")
+
     JSON = json.loads(content)
     n = 0
     while JSON["status"] == "error":
@@ -85,20 +86,62 @@ if __name__ == '__main__':
         if n == 10:
             print("获取失败，请重新输入")
             exit(0)
+    print(content)
     output = open('output.ics', 'w', encoding="utf-8")
-    output.write("BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//YZune//WakeUpSchedule//EN\n")
+    output.write("BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//HHR//ZJUTClass2ICS//EN\n")
     for each in JSON["msg"]:
         ClassName = each["kcmc"]
         Classroom = each["cdmc"]
         Teacher = each["xm"]
-        week = each["zcd"][:-1]
-        while not ('0' <= week[-1] <= '9'):  # 剪掉后面的所有中文
-            week = week[:-1]
-        week = week + "-" + week[0]  # 避免某课程只上一周导致的异常
         weekday = each["xqj"]
         LessonTime = each["jcor"]
         Campus = each["xqmc"]
+        week = each["zcd"][:-1]
+        flag = 0
+        try:
+            tmp = week.index("单")
+        except ValueError:
+            flag = 1
+        if flag == 0:
+            week = week[:-3]
+            startWeek = int(week.split("-")[0])
+            endWeek = int(week.split("-")[1])
+            for i in range(startWeek, endWeek):
+                if i % 2 == 0:
+                    out(ClassName, Classroom, Teacher, str(i) + "-" + str(i), weekday, LessonTime, Campus, FirstDay,
+                        output)
+            continue
+        flag = 0
+        try:
+            tmp = week.index("双")  # 处理形如 2-16周(双) 的情况
+        except ValueError:
+            flag = 1
+        if flag == 0:
+            week = week[:-3]
+            startWeek = int(week.split("-")[0])
+            endWeek = int(week.split("-")[1])
+            for i in range(startWeek, endWeek):
+                if i % 2 != 0:
+                    out(ClassName, Classroom, Teacher, str(i) + "-" + str(i), weekday, LessonTime, Campus, FirstDay,
+                        output)
+            continue
+        try:
+            week1 = week.split(",")[1]  # 处理形如 2-8周,10-16周 的情况
+        except IndexError:
+            while not ('0' <= week[-1] <= '9'):  # 剪掉后面的所有中文
+                week = week[:-1]
+            week = week + "-" + week[0]  # 避免某课程只上一周导致的异常
+            out(ClassName, Classroom, Teacher, week, weekday, LessonTime, Campus, FirstDay, output)
+            continue
+        week = week.split(",")[0]
+        while not ('0' <= week[-1] <= '9'):  # 剪掉后面的所有中文
+            week = week[:-1]
+        week = week + "-" + week[0]  # 避免某课程只上一周导致的异常
         out(ClassName, Classroom, Teacher, week, weekday, LessonTime, Campus, FirstDay, output)
+        while not ('0' <= week1[-1] <= '9'):  # 剪掉后面的所有中文
+            week1 = week1[:-1]
+        week1 = week1 + "-" + week1[0]  # 避免某课程只上一周导致的异常
+        out(ClassName, Classroom, Teacher, week1, weekday, LessonTime, Campus, FirstDay, output)
     output.write("END:VCALENDAR")
     output.close()
     print("文件生成成功")
